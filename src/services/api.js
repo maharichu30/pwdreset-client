e21573f95b75962d4bc407bbb3fcf5d30e0b96b9
maharichu30 <rajmaharichu30@gmail.com>
2026-03-25 06:16:45 -0600

    completed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://pwdreset-server.onrender.com/api"
+  baseURL: "https://pwdreset-server.netlify.app/api"
 });
 
 export default API;
