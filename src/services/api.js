1ebec7ba1195d11b048ec31e2233265c12dc1146
maharichu30 <rajmaharichu30@gmail.com>
2026-03-25 06:55:35 -0600

    COMPLETED

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://pwdreset-server.netlify.app/api"
+  baseURL: "https://pwdreset-server-production.up.railway.app/api"
 });
 
 export default API;
